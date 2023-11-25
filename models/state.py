85656b464368d42e49bd21de0b46c5e7f7199c2a
Mugisha Goal <degaullemugisha@gmail.com>
2023-11-25 14:51:51 -0800

    Updated state

--- models/state.py
+++ models/state.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
+"""The State class"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from os import getenv
+from sqlalchemy import Column, Integer, String
+from models.city import City
 import models
+import shlex
 
 
 class State(BaseModel, Base):
-    """State class"""
-    __tablename__ = 'states'
-
+    """The State class
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    
-    # For DBStorage
-    cities = relationship("City", cascade='all, delete', back_populates="state")
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    # For FileStorage
     @property
     def cities(self):
-        """Getter attribute for cities in FileStorage"""
-        from models import storage
-        city_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+        """The method of updating cities"""
+        var = models.storage.all()
+        lst = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lst.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
