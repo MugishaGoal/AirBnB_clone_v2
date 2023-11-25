9b4ec62cca4dca4d49fcb44fd75d2707c92335b3
Mugisha Goal <degaullemugisha@gmail.com>
2023-11-25 12:36:52 -0800

    Updated State

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class"""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    
+    # For DBStorage
+    cities = relationship("City", cascade='all, delete', back_populates="state")
+
+    # For FileStorage
+    @property
+    def cities(self):
+        """Getter attribute for cities in FileStorage"""
+        from models import storage
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
