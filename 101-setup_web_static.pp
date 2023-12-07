2c9decb616fd55bae4e7fd33b3fa0d02ef589a19
Mugisha Goal <degaullemugisha@gmail.com>
2023-12-07 15:40:42 -0800

    A Puppet script tha sets p web servers for the deployment of web_static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,66 +1,102 @@
-# 101-setup_web_static.pp
+# A Puppet script that sets up the web servers for the deployment of web_static
 
-# Install Nginx package
+# Nginx configuration content
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://linktr.ee/firdaus_h_salim/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+# Ensure Nginx is installed
 package { 'nginx':
-  ensure => 'installed',
+  ensure   => 'present',
+  provider => 'apt'
 }
 
-# Create necessary directories
+# Create necessary directories for web server setup
 file { '/data':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  ensure  => 'directory'
 }
 
 file { '/data/web_static':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  ensure => 'directory'
 }
 
 file { '/data/web_static/releases':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-file { '/data/web_static/shared':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  ensure => 'directory'
 }
 
 file { '/data/web_static/releases/test':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  ensure => 'directory'
 }
 
-# Create a fake HTML file
+file { '/data/web_static/shared':
+  ensure => 'directory'
+}
+
+# Create a sample HTML file for testing
 file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  content => '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+  ensure  => 'present',
+  content => "this webpage is found in data/web_static/releases/test/index.htm\n"
 }
 
-# Create or recreate the symbolic link
+# Create or update the symbolic link to the current release
 file { '/data/web_static/current':
-  ensure  => 'link',
-  target  => '/data/web_static/releases/test',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
 }
 
-# Update Nginx configuration
+# Ensure correct ownership of the /data/ directory
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+
+# Create directories for Nginx web server
+file { '/var/www':
+  ensure => 'directory'
+}
+
+file { '/var/www/html':
+  ensure => 'directory'
+}
+
+# Create sample HTML files for Nginx web server
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "This is my first upload in /var/www/index.html***\n"
+}
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page - Error page\n"
+}
+
+# Update Nginx configuration file
 file { '/etc/nginx/sites-available/default':
   ensure  => 'file',
-  content => "server {\n\tlisten 80 default_server;\n\tserver_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n\n\tlocation / {\n\t\t# your other configurations...\n\t}\n}\n",
+  content => $nginx_conf,
   notify  => Exec['nginx_restart'],
 }
 
 # Restart Nginx when the configuration is updated
 exec { 'nginx_restart':
-  command     => 'service nginx restart',
+  command => '/etc/init.d/nginx restart',
   refreshonly => true,
 }
