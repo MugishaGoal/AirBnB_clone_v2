af3266c7e5c5c3ff1d07fb4decec602873724eb0
Mugisha Goal <degaullemugisha@gmail.com>
2023-12-07 12:07:44 -0800

    A  Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to the web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,55 +1,31 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers, using the
-function do_deploy
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
+
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['<100.26.230.60>', '52.91.183.162']
+env.hosts = ['100.26.230.60', '52.91.183.162']
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to the web servers.
-
-    Args:
-        archive_path (str): The path to the archive file.
-
-    Returns:
-        bool: True if successful, False otherwise.
-    """
-    """Check if the archive exists"""
-    if not exists(archive_path):
+    """Distribute an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        """Extract necessary information from the archive_path"""
-        file_name = archive_path.split("/")[-1]
-        base_name = file_name.split(".")[0]
-        release_path = "/data/web_static/releases/"
-
-        """Upload the archive to /tmp/"""
+        file_split = archive_path.split("/")[-1]
+        pa_extract = file_split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-
-        """Create necessary directories and extract archive"""
-        run('mkdir -p {}{}/'.format(release_path, base_name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, release_path, base_name))
-
-        """Remove the temporary archive"""
-        run('rm /tmp/{}'.format(file_name))
-
-        """Move contents and create symbolic link"""
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(release_path, base_name))
-        run('rm -rf {}{}/web_static'.format(release_path, base_name))
+        run('mkdir -p {}{}/'.format(path, pa_extract))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_split, path, pa_extract))
+        run('rm /tmp/{}'.format(file_split))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, pa_extract))
+        run('rm -rf {}{}/web_static'.format(path, pa_extract))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(release_path, base_name))
-
-        """Print a success message"""
-        print("New version deployed!")
-
+        run('ln -s {}{}/ /data/web_static/current'.format(path, pa_extract))
         return True
-
     except Exception as e:
-        """Print an error message and return False"""
         print("Error during deployment: {}".format(e))
         return False
