b83e8ac118f5c9a224896ea573f542bf8aa09e4a
Mugisha Goal <degaullemugisha@gmail.com>
2024-01-03 13:00:33 +0000

    A script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+'''A simple Flask web application'''
 from flask import Flask
+
+
 app = Flask(__name__)
+'''A Flask application instance'''
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/", strict_slashes = False)
 def hello_hbnb():
+    '''A home page'''
     return "Hello HBNB!"
 
 
