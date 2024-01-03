11a2180c5db75ab79ce609fc3956b567253f6936
Mugisha Goal <degaullemugisha@gmail.com>
2024-01-03 13:20:30 +0000

    A script that starts a Flask web application with 2 routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,10 +6,12 @@ from flask import Flask
 app = Flask(__name__)
 '''A Flask application instance'''
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     '''Route to display "Hello HBNB!"'''
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
